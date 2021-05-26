4eb122f0257ce44364d462ffe1974917cdbfce7b
Prabesh789 <raiprabesh789@gmail.com>
2021-05-26 09:46:21 +0545

    second commit for

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,9 +1,22 @@
 import 'package:complex_json_serilization/constants/app_text.dart';
+import 'package:complex_json_serilization/model/user_model.dart';
 import 'package:flutter/material.dart';
+//  import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
+}
+
+//Boilerplate
+String url;
+List data = [];
+var response;
+List<User> userList;
+
+Future getUsers() async {
+  url = "http://jsonplaceholder.typicode.com/users";
+  // response = await
 }
 
 class _HomePageState extends State<HomePage> {
